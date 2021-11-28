98f78d68d9245280cc6321bd9dac69fe73865409
FabriJuncal <fabriciojuncal@micam.com.ar>
2021-11-28 17:01:56 -0300

    111. Página de encabezados

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+    // Declaramos un "ViewChild" para controlar un componente hijo, en este caso el "IonSegment"
+    @ViewChild(IonSegment) segment: IonSegment;
+    publisher = '';
+
+  // Tipos de categorias obtenidas de la API "https://newsapi.org/"
+  categorias = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+
   constructor() {}
+
+  // Función de Ionic que se ejecuta el terminar de mostrar toda la página
+  ionViewDidEnter() {
+
+    // Seleccionamos la 1ra opción como predeterminada en las opciones seleccionables del encabezado
+    this.segment.value = this.categorias[0];
+  }
 
 }
