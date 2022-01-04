64eeede58a69ac95cfe7990844a92f4f4868dbe3
FabriJuncal <juncalfabri@gmial.com>
2022-01-04 00:50:54 -0300

    Fix y Configuraciones para el Deploy en GitPages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiKey: '8936c90d112d4bd1a0fcc298fcd39bec'
+  apiKey: '8936c90d112d4bd1a0fcc298fcd39bec',
+  apiUrl: 'https://newsapi.org/v2'
 };
